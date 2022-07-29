4b918660d45cc389a90486dd33b55c1ddae8f5f9
Pivacarlos <103151488+Pivacarlos@users.noreply.github.com>
2022-07-29 15:00:26 -0300

    Commit 1.2

--- automação_serverest/features/step_definitions/cadastrar_produto.rb
+++ automação_serverest/features/step_definitions/cadastrar_produto.rb
@@ -12,4 +12,14 @@
     @cadastroProduto = CadastroProduto.new 
     end
   
-
+  Quando('o usuário preencher os campos {string}, {string}, {string} e {string}') do |string, string2, string3, string4|
+    @cadastroProduto.name.set string
+    @cadastroProduto.price.set string2 
+    @cadastroProduto.description.set string3 
+    @cadastroProduto.quantity.set string4 
+    @cadastroProduto.btn_cadastrar.click
+  end
+  
+  Então('o Sistema Exibe a {string}') do |string|
+    expect(@cadastroProduto).to have_content string 
+  end
